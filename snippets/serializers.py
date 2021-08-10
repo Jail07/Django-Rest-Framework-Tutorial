437e335e7f7328e6b556e1f1d739dcd456f79fd2
Jail <jailds07@gmail.com>
2021-08-10 17:41:31 +0600

    4th part

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from snippets.models import Snippet
+from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        model = User
+        fields = ['id', 'username', 'snippets']
